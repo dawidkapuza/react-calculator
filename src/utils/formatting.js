b8732aacb7f65492420c201e282a7e7df880f894
Dawid Wade <davidwade064@gmail.com>
2022-04-18 15:02:33 +0200

    Compatibilty with Safari

--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -2,7 +2,7 @@ export const toLocaleString = (num) =>
   num
     ? num
         .toString()
-        .replace(/(?<=\D)0$/, "")
+        .replace(/[^\D]*0$/, "")
         .replace("Infinity", "∞")
         .replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\D|$))/g, "$1 ")
     : "";
